5bf4d0410d431554fc68a6cb2b12aa2f432ba3c3
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-23 10:13:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
 import tkinter as tk
-from encrypt_pass import *
+from tkinter import messagebox
 
-def make_login():
-    pass
+#
+CORRECT_LOGIN = "admin"
+CORRECT_PASSWORD = "Nala00110111#"
+CORRECT_ENCRYPTION_PASSWORD = "8181160"
 
-# 
+#
+def perform_login():
+    username = entry_username.get()
+    password = entry_password.get()
+    encryption_password = entry_encryption_password.get()
+
+    #
+    if username == CORRECT_LOGIN and password == CORRECT_PASSWORD and encryption_password == CORRECT_ENCRYPTION_PASSWORD:
+        messagebox.showinfo("Success", "Login Successful!")
+
+    #
+    else:
+        messagebox.showerror("Error", "Incorrect username or password!")
+
+#
 root = tk.Tk()
-root.title("Login")
-root.geometry("400x200")
+root.title("Login Screen")
+root.geometry("400x150")
 root.iconbitmap("img/criptografia-de-dados.ico")
 
-# 
-label_login = tk.Label(root, text="Login: ")
-label_login.place(x=50, y=50)  
-entry_login = tk.Entry(root)
-entry_login.place(x=100, y=50)  
+#
+label_username = tk.Label(root, text="Username: ")
+label_username.place(x=50, y=50)
+label_username.pack()
+entry_username = tk.Entry(root)
+entry_username.pack()
 
-label_pass = tk.Label(root, text="Password: ")
-label_pass.place(x=50, y=100)  
-entry_pass = tk.Entry(root, show="*")
-entry_pass.place(x=100, y=100)  
+#
+label_password = tk.Label(root, text="Pass:")
+label_password.place(x=50, y=100)
+label_password.pack()
+entry_password = tk.Entry(root, show="*")
+entry_password.pack()
 
-button_login = tk.Button(root, text="Login", command=make_login)
-button_login.place(x=150, y=150)  
+#
+label_encryption_password = tk.Label(root, text="Encryption Pass")
+label_encryption_password.place(x=50, y=150)
+label_encryption_password.pack()
+entry_encryption_password = tk.Entry(root, show="*")
+entry_encryption_password.pack()
+
+#
+button_login = tk.Button(root, text="Login", command=perform_login)
+button_login.place(x=150, y=200)
+button_login.pack()
+
 root.mainloop()
