bb3005b29fd72a7014c12f43b321ac6cd43b27d2
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-23 09:28:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import tkinter as tk
+from encrypt_pass import *
 
-def fazer_login():
-    # Aqui você pode verificar se o login e a senha estão corretos
-    # Se estiverem corretos, você pode chamar a função para abrir a próxima tela
+def make_login():
     pass
 
-# Criando a janela principal
+# 
 root = tk.Tk()
 root.title("Login")
 root.geometry("400x200")
-root.iconbitmap("img/criptografia-de-dados.ico")  # Definindo as dimensões da janela
+root.iconbitmap("img/criptografia-de-dados.ico")
 
-# Criando os widgets
-label_login = tk.Label(root, text="Login:")
-label_login.place(x=50, y=50)  # Posicionando o widget na janela
+# 
+label_login = tk.Label(root, text="Login: ")
+label_login.place(x=50, y=50)  
 entry_login = tk.Entry(root)
-entry_login.place(x=100, y=50)  # Posicionando o widget na janela
+entry_login.place(x=100, y=50)  
 
-label_senha = tk.Label(root, text="Senha:")
-label_senha.place(x=50, y=100)  # Posicionando o widget na janela
-entry_senha = tk.Entry(root, show="*")
-entry_senha.place(x=100, y=100)  # Posicionando o widget na janela
+label_pass = tk.Label(root, text="Password: ")
+label_pass.place(x=50, y=100)  
+entry_pass = tk.Entry(root, show="*")
+entry_pass.place(x=100, y=100)  
 
-button_login = tk.Button(root, text="Login", command=fazer_login)
-button_login.place(x=150, y=150)  # Posicionando o widget na janela
-
+button_login = tk.Button(root, text="Login", command=make_login)
+button_login.place(x=150, y=150)  
 root.mainloop()
