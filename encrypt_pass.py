88d796e0613f0f8bc7c499bf6d10cdd1b928718d
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-23 10:41:05 -0300

    Update encrypt_pass.py

--- encrypt_pass.py
+++ encrypt_pass.py
@@ -4,7 +4,44 @@ from add_login_screen import *
 
 #
 def add_login():
-    pass
+    add_login_window = tk.Toplevel()
+    add_login_window.title("Add Login")
+    add_login_window.iconbitmap("img/criptografia-de-dados.ico")
+    add_login_window.geometry("300x200")
+
+    #
+    label_email = tk.Label(add_login_window, text="Email:")
+    label_email.pack()
+    entry_email = tk.Entry(add_login_window)
+    entry_email.pack()
+
+    #
+    label_password = tk.Label(add_login_window, text="Password:")
+    label_password.pack()
+    entry_password = tk.Entry(add_login_window, show="*")
+    entry_password.pack()
+
+    #
+    label_site = tk.Label(add_login_window, text="Site:")
+    label_site.pack()
+    entry_site = tk.Entry(add_login_window)
+    entry_site.pack()
+
+    #
+    def save_login():
+        email = entry_email.get()
+        password = entry_password.get()
+        site = entry_site.get()
+
+        print("Email: ", email)
+        print("Password: ", password)
+        print("Site: ", site)
+
+        add_login_window.destroy()
+    
+    #
+    btn_save = tk.Button(add_login_window, text="Save", command=save_login)
+    btn_save.pack()
 
 #
 def delete_login():
